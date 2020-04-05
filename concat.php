a2f46f0f1a6f4de872ebf9e6240ca785fa7fdf95
yuusiki54 <yuusiki54@icloud.com>
2020-04-05 22:28:57 +0900

    文字列

--- concat.php
+++ concat.php
@@ -1,8 +1,15 @@
 <?php
 
 //argvに入ってきた文字列を何度もechoさせる
-$letters = 0;
+$n = "";
+for($i = 1;$i < 100;$i++){
+    $n = $n.$argv[$i];
+}
+echo $n;
+// $n = $argv[1];
+// $num = $argv[2];
 
+// echo $n.$num;
 
 
 
