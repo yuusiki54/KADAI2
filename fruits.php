77c955e0c630632b06fbcc75ef30f40ef8040c2f
yuusiki54 <yuusiki54@icloud.com>
2020-04-07 21:51:40 +0900

    ”フルーツの値段”

---
+++ fruits.php
@@ -0,0 +1,44 @@
+<?php
+if("apple" === $argv[1]){
+    echo "200円";
+}elseif("banana" === $argv[1]){
+    echo "100円";
+}elseif("orange" === $argv[1]){
+    echo "150円";
+}elseif("grape" === $argv[1]){
+    echo "300円";
+}elseif("peach" === $argv[1]){
+    echo "400円";
+}
+
+
+
+
+// $fruits = $argv;
+// array_shift($fruits);
+// print_r($fruits);
+//$fruitsにはフルーツの配列が入る
+
+
+
+
+
+// $apple = $argv[1];
+// $banana = $argv[2];
+// $orange = $argv[3];
+// $grape = $argv[4];
+// $peach = $argv[5];
+// $fruits = [
+//     $apple => "200円",
+//     $banana => "100円",
+//     $orange => "150円",
+//     $grape => "300円",
+//     $peach => "400円",
+// ];
+// if($apple === "ringo"){
+//     echo $fruits[$apple];
+// }elseif($banana === "banana"){
+//     echo $fruits[$banana];
+// }
+// 
+?>
