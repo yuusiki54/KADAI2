994ab2b5e0113f2356cdd364bf510c51ef242243
yuusiki54 <yuusiki54@icloud.com>
2020-04-09 21:04:53 +0900

    フルーツの名前を複数打つと値段を合計して平均を出すプログラム

--- fruitsavg.php
+++ fruitsavg.php
@@ -11,12 +11,30 @@
 // 実行例） php fruits.php りんご ぶどう もも --> 平均 300円
 
 $fruits = ["りんご" => 200,"バナナ" => 100,"みかん" => 150,"ぶどう" => 300,"もも"  => 400];
-
+$total_price = 0;
+$total_count = 0;
 foreach($fruits as $kudamono => $price){
-    
-    
-
+    if($argv[1] == $kudamono){
+        $total_price += $price;
+        $total_count += 1;
+    }elseif($argv[2] == $kudamono){
+        $total_price += $price;
+        $total_count += 1;
+    }elseif($argv[3] == $kudamono){
+        $total_price += $price;
+        $total_count += 1;
+    }elseif($argv[4] == $kudamono){
+        $total_price += $price;
+        $total_count += 1;
+    }elseif($argv[5] == $kudamono){
+        $total_price += $price;
+        $total_count += 1;
     }
+}
+
+$fruitsavg = $total_price / $total_count;
+echo $fruitsavg;
+
 
 
 ?>
